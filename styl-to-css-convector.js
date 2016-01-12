815d991e89d72923876d7b2f71f2fbf43608a7e5
Vladislav Khvostov <khvostov@yamoney.ru>
2016-01-12 19:47:12 +0300

    first commit

---
+++ styl-to-css-convector.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var stylus = require('stylus');
+var glob = require("glob")
+
+// options is optional 
+glob("blocks/**/*.styl", function (er, files) {
+	// Для каждого файла.
+	files.forEach(function (file) {
+		// Читаем содержимое
+		fs.readFile(file, function (err, data) {
+			var content = data.toString()
+
+			// Компилируем содержимое в css.
+			stylus.render(content, {}, function(err, css){
+				if (err) throw err;
+
+				// Ложим css в файл
+				fs.writeFile(file, css, function () {
+					console.log(' File ' + file + ' was rewrited');
+				});
+				// Переминовываем файл.
+				fs.rename(file, file.replace('.styl', '.css'), function () {
+					console.log(' File ' + file + ' was changed extension');
+				});
+			});
+
+		});
+
+	})
+})
+
